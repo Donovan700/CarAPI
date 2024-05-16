380bb3784ebfa9854a6758721f9299c9cfd44748
Donovan700 <bryanrakotosedson@gmail.com>
2024-05-16 20:27:57 +0300

    fix: privacy and security

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,7 +10,7 @@ export class MailService {
       service: 'gmail',
       auth: {
         user: 'yourmail@gmail.com',
-        pass: 'ntum zwco iyrp mzyd',
+        pass: 'yourPassword',
       },
     });
   }
